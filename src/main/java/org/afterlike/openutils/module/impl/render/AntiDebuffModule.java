bd20a2b287f85ff36e630c146aea90d9201172d6
HugoQwQ <guhugo@qq.com>
2025-12-28 10:06:03 +0800

    feat(antidebuff): Implement AntiDebuffModule

--- src/main/java/org/afterlike/openutils/module/impl/render/AntiDebuffModule.java
+++ src/main/java/org/afterlike/openutils/module/impl/render/AntiDebuffModule.java
@@ -1,5 +1,7 @@
 package org.afterlike.openutils.module.impl.render;
 
+import net.minecraft.potion.Potion;
+import org.afterlike.openutils.event.handler.EventHandler;
 import org.afterlike.openutils.module.api.Module;
 import org.afterlike.openutils.module.api.ModuleCategory;
 import org.afterlike.openutils.module.api.setting.impl.BooleanSetting;
@@ -11,5 +13,18 @@ public class AntiDebuffModule extends Module {
 		super("Anti Debuff", ModuleCategory.RENDER);
 		nausea = this.registerSetting(new BooleanSetting("Remove nausea", true));
 		blindness = this.registerSetting(new BooleanSetting("Remove blindness", true));
+	}
+
+	@EventHandler
+	private void onUpdate() {
+		if (mc.thePlayer == null) return;
+
+		if (nausea.getValue() && mc.thePlayer.isPotionActive(Potion.confusion)) {
+			mc.thePlayer.removePotionEffectClient(Potion.confusion.getId());
+		}
+
+		if (blindness.getValue() && mc.thePlayer.isPotionActive(Potion.blindness)) {
+			mc.thePlayer.removePotionEffectClient(Potion.blindness.getId());
+		}
 	}
 }
